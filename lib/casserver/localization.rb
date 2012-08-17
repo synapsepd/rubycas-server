256c8f410c35cc639d2b1a55283dcdaed5b8fc28
Adam Saegebarth <adams@synapse.com>
2012-08-16 23:06:32 -0700

    fix localization path

--- lib/casserver/localization.rb
+++ lib/casserver/localization.rb
@@ -5,7 +5,7 @@ module CASServer
     def self.included(mod)
       mod.module_eval do
         register Sinatra::R18n
-        R18n.default_places { './locales' }
+        R18n.default_places { File.dirname(__FILE__) + '/../locales' }
         set :default_locale, 'en'
         #set :translations, File.dirname(__FILE__) + "/../locales"
       end
