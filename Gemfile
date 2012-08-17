6188514e4aac99a7de59ce5722ccb2b36b140ade
Adam Saegebarth <adams@synapse.com>
2012-08-16 22:42:22 -0700

    Capify 2

--- Gemfile
+++ Gemfile
@@ -1,3 +1,9 @@
 source "http://rubygems.org"
 gemspec
 
+gem 'capistrano'
+gem 'capistrano-ext'
+gem 'capistrano_colors'
+gem 'rvm-capistrano'
+gem 'mysql2'
+gem 'activerecord-mysql2-adapter'
---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+require "bundler/capistrano"
+require "capistrano/ext/multistage"
+require "rvm/capistrano"
+
+set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
+
+before 'deploy:setup', 'rvm:install_rvm'   # install RVM
+before 'deploy:setup', 'rvm:install_ruby'  # install Ruby and create gemset, or:
+before 'deploy:setup', 'rvm:create_gemset' # only create gemset
+
+set :stages, %w(development staging production)
+set :scm, :git
+set :deploy_via, :remote_cache
+set :user, 'cas-server-www'
+set :use_sudo, false
+set :ssh_options, { :forward_agent => true }
+default_run_options[:pty] = true
+
+set :application, "rubycas-server"
+set :repository,  "git@github.com:synapsepd/rubycas-server.git"
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
