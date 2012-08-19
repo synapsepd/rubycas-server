ba217ad24bedfd43be65be7fe88b8f931d3e4657
Adam Saegebarth <adams@synapse.com>
2012-08-18 21:28:16 -0700

    change serialize function

--- lib/casserver/server.rb
+++ lib/casserver/server.rb
@@ -737,14 +737,16 @@ module CASServer
     end
 
     def serialize_extra_attribute(builder, key, value)
+      puts 'run serialize' + value.to_s
       if value.kind_of?(String)
         builder.tag! key, value
       elsif value.kind_of?(Numeric)
         builder.tag! key, value.to_s
       else
-        builder.tag! key do
-          builder.cdata! value.to_yaml
-        end
+        builder.tag! key, value.to_s
+        # builder.tag! key do
+        #   builder.cdata! value.first
+        # end
       end
     end
 
